ebbf8d6ca3b3bfa43cce256d8d0e22b7b2201ea2
gabba23 <gabrieliuc11@gmail.com>
2019-10-30 13:11:13 +0100

    initial import

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP')}}</title>
+
+    </head>
+    <body>
+       <h1>Welcome to Laravel</h1>
+       <p>Tte yes</p>
+    </body>
+</html>
