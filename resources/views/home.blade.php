1ab5e2b9d2ec0dbc3b082baea2046077dc3b0395
Olsonika <alek2257@easv365.dk>
2019-11-11 14:49:56 +0100

    11112019

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Medicine list</div>
+                <div class="card-header">The medicine list</div>
 
                 <div class="card-body">
                     @if (session('status'))
